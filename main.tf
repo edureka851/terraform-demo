52d2633fb93dd69f8765d72af8a43f067b5057e9
edureka851 <madhav.ashrit@edureka.co>
2025-02-24 16:18:40 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Use the appropriate AMI ID for your region
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "ExampleInstance"
-  }
-}
-
